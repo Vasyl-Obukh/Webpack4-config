40fbf51da74262d6b419dd8cd2f4fa8a03f07fa0
Aidan_Odwyer <vasiaobukh7@gmail.com>
2018-12-18 21:16:06 +0200

    something go wrong

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,4 @@
+import "../img/red-dead-redemption-2-1.jpg";
+import '../css/style.css';
+
 document.getElementById('title').innerHTML = "Hello";
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,13 @@
 const path               = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const HtmlWebpackPlugin  = require("html-webpack-plugin");
-const ExtractTextPlugin  = require("extract-text-webpack-plugin");
+const HtmlWebpackPlugin  = require('html-webpack-plugin');
+const CopyWebpackPlugin  = require('copy-webpack-plugin');
+const ImageminPlugin     = require('imagemin-webpack-plugin').default;
+const ExtractTextPlugin  = require('extract-text-webpack-plugin');
 
 let conf = {
   entry: {
-    index: ['./src/js/index.js'/* , './src/sass/style.sass' */]
+    index: ['./src/js/index.js', './src/css/style.css' /* , './src/sass/style.sass' */]
   },
   output: {
     path: path.resolve(__dirname, './dist/js'),
@@ -38,8 +40,20 @@ let conf = {
         test: /\.css$/,
         use: ExtractTextPlugin.extract({
           //fallback: "style-loader",
-          use: "css-loader"
+          use: ['css-loader', 'postcss-loader']
         })
+      },
+      {
+        test: /\.(png|gif|jpe?g)$/,
+        loaders: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '../img/[name].[ext]'
+            }
+          },
+          'img-loader'
+        ]
       }
     ]
   },
@@ -47,9 +61,13 @@ let conf = {
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
       template: 'index.html',
-      filename: './dist/index.html'
+      filename: '../index.html'
     }),
-    new ExtractTextPlugin("./css/style.css")
+    new CopyWebpackPlugin([{ 
+      from: './src/img', 
+      to: '../img' 
+    }]),
+    new ExtractTextPlugin('../css/style.css'),
   ]
 };
 
@@ -57,5 +75,12 @@ let conf = {
 module.exports = (env, options) => {
   let production = options.mode ==='production';
   conf.devtool = production ? false/* 'source-map' */ : 'eval-sourcemap';
+  if (production) {
+    conf.plugins.push(
+      new ImageminPlugin({
+        test: /\.(png|jpe?g|gif|svg)$/i
+      }),
+    );
+  }
   return conf;
 }
